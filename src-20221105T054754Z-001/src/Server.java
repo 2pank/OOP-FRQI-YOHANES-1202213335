a24640c0ef6d726bb92758ae0bd0fcca747786db
2pank <115936950+2pank@users.noreply.github.com>
2022-11-05 13:44:59 +0700

    Add files via upload

---
+++ src-20221105T054754Z-001/src/Server.java
@@ -0,0 +1,35 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        // TODO Create Database
+
+        // TODO Create Menu
+        Menu menu1 = new Menu();
+        menu1.setName("Babi");
+        menu1.setCategory("Makanan");
+        menu1.setPrice(25000);
+
+        Menu menu2 = new Menu();
+        menu2.setName("Soda Gembira");
+        menu2.setCategory("Minuman");
+        menu2.setPrice(10000);
+
+        Menu menu3 = new Menu();
+        Menu3.setName("Kerupuk");
+        menu3.setCategory("Cemilan");
+        menu3.setPrice(1000);
+
+
+
+        // TODO Insert Menu to Database
+
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
---
+++ src-20221105T054754Z-001/src/User.java
@@ -0,0 +1,24 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String name;
+    int phoneNumber;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public oid setphoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+    public void Register(){
+        sytem.out.println(x="==================")
+        system.out.println("Name= "+name)
+        system.out.println("phoneNumber= "+phoneNumber)
+        system.out.println(x="Success")
+
+    }
+
+}
